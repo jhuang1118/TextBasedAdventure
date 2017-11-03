593e032913ac3ef03826f00a18010dbc6852f217
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-03 14:32:53 -0400

    Updated

---
+++ Text Adventure/src/ethanDavidMinigame/DavidRoom.java
@@ -0,0 +1,12 @@
+package ethanDavidMinigame;
+
+import caveExplorer.NPCRoom;
+
+public class DavidRoom extends NPCRoom {
+
+	public DavidRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
