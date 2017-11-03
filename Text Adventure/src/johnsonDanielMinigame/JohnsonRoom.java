e47c4b1b17cb55d0be54c0c32127f0de26f91ece
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-11-03 14:35:01 -0400

    package

---
+++ Text Adventure/src/johnsonDanielMinigame/JohnsonRoom.java
@@ -0,0 +1,12 @@
+package johnsonDanielMinigame;
+
+import caveExplorer.NPCRoom;
+
+public class JohnsonRoom extends NPCRoom {
+
+	public JohnsonRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
