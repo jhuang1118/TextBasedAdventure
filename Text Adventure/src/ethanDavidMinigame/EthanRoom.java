0af9b1c1fe92ffa0afbe96c1708c3cb6bae1cc45
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-11-03 14:58:14 -0400

    added room

---
+++ Text Adventure/src/ethanDavidMinigame/EthanRoom.java
@@ -0,0 +1,10 @@
+package ethanDavidMinigame;
+
+import caveExplorer.NPCRoom;
+
+public class EthanRoom extends NPCRoom{
+	public EthanRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+}
