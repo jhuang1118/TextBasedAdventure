45601075d7d1e448b79bea1e4b01aa196622eeca
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-11-03 15:29:27 -0400

    updates

--- Text Adventure/src/jasonYJasonZMinigame/JasonZGuns.java
+++ Text Adventure/src/jasonYJasonZMinigame/JasonZGuns.java
@@ -7,9 +7,9 @@ public class JasonZGuns {
 	private int ammo;
 	private int rateOfFire;
 	private String name;
-				//auto		pistol		smg 
+				//auto		pistol		sub machine gun 
 	public final static int[][] TYPE = {{4,10,30,4},{2,4,8,3},{3,3,20,4}};
-	
+	//range, damage, magazines, fire rate 
 	public JasonZGuns(String[] description) {
 		name = description[0];
 		
