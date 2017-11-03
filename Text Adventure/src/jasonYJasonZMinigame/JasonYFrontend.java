d1e5a006b27330f94bdb4c9ca6673f4177ded99c
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-11-03 14:44:34 -0400

    created Package

---
+++ Text Adventure/src/jasonYJasonZMinigame/JasonYFrontend.java
@@ -0,0 +1,12 @@
+package jasonYJasonZMinigame;
+
+import caveExplorer.JasonJason;
+
+public class JasonYFrontend extends JasonJason {
+
+	public JasonYFrontend(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
