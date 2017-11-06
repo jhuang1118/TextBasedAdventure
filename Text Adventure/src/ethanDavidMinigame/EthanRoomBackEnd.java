581d5ec484bce815b45d21adc74d9a3ec3d9963e
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-11-06 14:40:02 -0500

    update

--- Text Adventure/src/ethanDavidMinigame/EthanRoomBackEnd.java
+++ Text Adventure/src/ethanDavidMinigame/EthanRoomBackEnd.java
@@ -2,6 +2,7 @@ package ethanDavidMinigame;
 
 import caveExplorer.CaveExplorer;
 import caveExplorer.CaveRoom;
+import caveExplorer.NPC;
 import caveExplorer.NPCRoom;
 
 public class EthanRoomBackEnd extends NPCRoom{
@@ -9,5 +10,35 @@ public class EthanRoomBackEnd extends NPCRoom{
 		super(description);
 		// TODO Auto-generated constructor stub
 	}
-	
-}
+		public void printValidMoves() {
+			System.out.println("You can only enter 'w', 'a', 's', or 'd' to move " + 
+					"or you can press 'e' to interact.");
+			
+		}
+		public String validMoves() {
+			return "wdsae";
+		}
+		/**
+		 * override to create response to keys other than wdsa
+		 * @param direction
+		 */
+		public void performAction(int direction) {
+			if(direction == 5) {
+				interact();
+			}else {
+				super.performAction(direction);
+				
+			}		
+		}
+		private void interact() {
+			//int n = caveExplorer.Inventory.getMoney();
+			
+		}
+		public String getContents() {
+			return "V";
+		}
+		
+		public String getDescription() {
+			return "You have entered the vault. Enter using 'e'";
+		}
+	}
