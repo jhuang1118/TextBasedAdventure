07b569e2eef4b03223cf1d8460e8612d0226e6e7
Lenovo <Lenovo@Jenny>
2017-11-07 14:34:35 -0500

    Updated

--- Text Adventure/src/ethanDavidMinigame/DavidRoomFrontEnd.java
+++ Text Adventure/src/ethanDavidMinigame/DavidRoomFrontEnd.java
@@ -11,8 +11,16 @@ public class DavidRoomFrontEnd extends NPCRoom {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public String getSymbol() {
+		return "P";
+	}
+	
 	public String spawnPolice() {
 		int numberOfPolice = (int)(Math.random() * 5) + 3;
+		for(int i = 0; i < numberOfPolice; i++) {
+			CaveRoom[][] c = CaveExplorer.caves;
+			int index = (int)(Math.random() * c.length);
+		}
 	}
 	
 }
