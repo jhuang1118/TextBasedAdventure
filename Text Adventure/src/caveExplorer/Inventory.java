c16207952a322989589783d5036656f9d33be44a
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-11-10 11:20:19 -0500

    update

--- Text Adventure/src/caveExplorer/Inventory.java
+++ Text Adventure/src/caveExplorer/Inventory.java
@@ -13,7 +13,6 @@ public class Inventory {
 		ID = iD;
 	}
 
-	private int money;
 	private String map;
 
 	private int cash;
@@ -23,16 +22,9 @@ public class Inventory {
 	private static int hp;
 
 
-	public int getMoney() {
-		return money;
-	}
-	public void setMoney(int money) {
-		this.money = money;
-	}
 	public Inventory()
 	{
-		cash = 0;
-		setMoney(0);
+		setCash(0);
 		updateMap(CaveExplorer.caves);
 	}
 	
@@ -43,30 +35,18 @@ public class Inventory {
 		this.cash = cash;
 	}
 	public void updateMap(CaveRoom[][] caves) {
-
-	
-	public void updateMap() {
 		map = " ";
 		// make for. line across top:
 		for(int i = 0; i< caves[0].length -1; i++)
 		{
 			map += "____";//4
-
-		//make hor. line across top:
-
+		}
 		map +="___ \n";//3
 		for(CaveRoom[] row: caves)
 		{
 			for(int i = 0; i< 3; i++)
-
-		//for symmetry, add only three underscores:
-		map += "___\n";
-		for(CaveRoom[] row: CaveExplorer.caves) {
-			//three lines of text per room
-			for(int i = 0; i < 3; i++) {
-
+			{
 				String text = "";
-
 				for(CaveRoom cr: row)
 				{
 					//if a wall is present draw a line otherwise draw a \
@@ -76,38 +56,42 @@ public class Inventory {
 					}
 					else
 					{
-
-				for(CaveRoom cr: row) {
-					//if a wall is present draw a line otherwise draw a "\"
-					if(cr.getDoor(CaveRoom.WEST) != null &&
-							cr.getDoor(CaveRoom.WEST).isOpen()) {
-						text += " ";
-					}else {
-
 						text += "|";
 					}
-					if(i ==0) {
-						text += " ";
-					}else if(i == 1) {
-						text += " " + cr.getContents() + " ";
-					}else if(i == 2) {
-						if(cr.getDoor(CaveRoom.SOUTH) != null && 
-								cr.getDoor(CaveRoom.SOUTH).isOpen()) {
-							text += "   ";//3 spaces
-						}else {
-							text += "___";//3 underscores(closed door or wall)
+					if(i == 0)
+					{
+						text += "   ";
+					}
+					else
+					{
+						if(i ==1)
+						{
+							text += " "+ cr.getContents() + " ";
+						}
+						else
+						{
+							if(i ==2)
+							{
+								if(cr.getDoor(CaveRoom.SOUTH) != null && cr.getDoor(CaveRoom.SOUTH).isOpen())
+								{
+									text += "   ";
+								}
+								else
+								{
+									text += "___";
+								}
+							}
 						}
 					}
-				}//last caveroom in row
+				}
 				text += "|";
-				map += text + "\n";
-					}
+				map += text +"\n";
+			}
+		}
 	}
-
-
 	public String getDescription()
 	{
-		return map + "\n" + "Also you have " + getMoney() + " money \n";
+		return map + "\n" + "Also you have " + getCash() + " cash \n";
 	}
 	public void updateHP() {
 		
