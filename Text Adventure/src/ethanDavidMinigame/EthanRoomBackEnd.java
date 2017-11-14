f8fdcd2ecf75f871deb4a44a5bc06de96c477251
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-14 14:50:53 -0500

    update

--- Text Adventure/src/ethanDavidMinigame/EthanRoomBackEnd.java
+++ Text Adventure/src/ethanDavidMinigame/EthanRoomBackEnd.java
@@ -12,7 +12,39 @@ public class EthanRoomBackEnd implements DavidSupport{
 		this.frontend = frontend;
 	}
 	
+	public boolean hitLaser() {
+		return false;
+	}
+	
+	public void generateCash() {
+		
+	}
+	
 	public boolean isExited() {
 		return false;
+	}
+
+	@Override
+	public boolean stillPlaying() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getValidUserInput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void startTimer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object victorious() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
