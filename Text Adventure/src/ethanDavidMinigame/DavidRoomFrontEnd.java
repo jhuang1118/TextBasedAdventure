3457dfe5437724fd0698e6314de14f44ccc9ccdb
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-14 14:40:47 -0500

    Updated

--- Text Adventure/src/ethanDavidMinigame/DavidRoomFrontEnd.java
+++ Text Adventure/src/ethanDavidMinigame/DavidRoomFrontEnd.java
@@ -12,6 +12,30 @@ public class DavidRoomFrontEnd implements EthanSupport {
 	}
 	
 	private void play() {
+		while(backend.stillPlaying()) {
+			displayBoard();
+			String input = backend.getValidUserInput();
+			respondToInput(input);
+			backend.startTimer();
+		}
+		printGameOverMessage(backend.victorious());
+		
+	}
+	
+	private void printGameOverMessage(Object victorious) {
+		System.out.println("You're dead.");
+	}
+
+	public void displayTimer() {
+		System.out.println("");
+	}
+
+	public void respondToInput(String input) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void displayBoard() {
 		// TODO Auto-generated method stub
 		
 	}
--- Text Adventure/src/ethanDavidMinigame/DavidSupport.java
+++ Text Adventure/src/ethanDavidMinigame/DavidSupport.java
@@ -2,4 +2,12 @@ package ethanDavidMinigame;
 
 public interface DavidSupport {
 
+	boolean stillPlaying();
+
+	String getValidUserInput();
+
+	void startTimer();
+
+	Object victorious();
+
 }
