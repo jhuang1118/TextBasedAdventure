d14a8f9a436946653628a41407230595e6da833c
Lenovo <Lenovo@Jenny>
2017-11-16 22:24:57 -0500

    Updated

--- Text Adventure/src/ethanDavidMinigame/DavidRoomFrontEnd.java
+++ Text Adventure/src/ethanDavidMinigame/DavidRoomFrontEnd.java
@@ -1,10 +1,16 @@
 package ethanDavidMinigame;
 
+import java.util.Scanner;
+
 public class DavidRoomFrontEnd implements EthanSupport {
 	
 	private DavidEthanRoom[][] rooms;
 	private int currentRow;
 	private int currentCol;
+	
+	long startTime = System.currentTimeMillis();
+	long elapsedTime = System.currentTimeMillis() - startTime;
+	long elapsedSeconds = elapsedTime/1000;
 	
 	private DavidSupport backend;
 	
@@ -12,6 +18,8 @@ public class DavidRoomFrontEnd implements EthanSupport {
 	public static final int EAST = 1;
 	public static final int SOUTH = 2;
 	public static final int WEST = 3;
+	
+	public static Scanner in;
 	
 	public static void main(String[] args) {
 		DavidRoomFrontEnd game = new DavidRoomFrontEnd();
@@ -35,18 +43,26 @@ public class DavidRoomFrontEnd implements EthanSupport {
 	public void displayTimer() {
 		System.out.println("You have 40 seconds to collect money!");
 	}
-
+	
 	public void respondToInput(String input) {
-		// TODO Auto-generated method stub
-		
+		while(!isValid(input)) {
+			System.out.println("You can not do that. You must type 'w,a,s, or d.' You still have " + (40 - elapsedSeconds) + " seconds left.");
+			input = in.nextLine();
+		}
+		int direction = validMoves().indexOf(input);
+	}
+	
+	public String validMoves() {
+		return "wdsa";
+	}
+	
+	private boolean isValid(String input) {
+		return validMoves().indexOf(input) > -1 && input.length() == 1;
 	}
 
 	public void displayBoard() {
-		//spawn cash piles (randomly)
-		//spawn obstacles (randomly)
-		//if they choose the same random location choose somewhere else
 		for(int row = 0; row < rooms.length; row++) {
-			for(int col = 0; col < rooms[row].length; col++){
+			for(int col = 0; col < rooms[row].length; col++) {
 				if(row == currentRow && col == currentCol) {
 					System.out.print("X");
 				}
@@ -58,9 +74,13 @@ public class DavidRoomFrontEnd implements EthanSupport {
 		}
 		}	
 	
+	public void updateBoard() {
+		
+	}
+	
 	public DavidRoomFrontEnd() {
 		backend = new EthanRoomBackEnd(this);
-		rooms = new DavidEthanRoom[10][10];
+		rooms = new DavidEthanRoom[5][15];
 		for(DavidEthanRoom[] row: rooms) {
 			for(int col = 0; col < row.length; col++) {
 				row[col] = new DavidEthanRoom();
