159eec0bc031f9fe52bccd16ac0b01a099b51c3c
Ethan Tam <etam4225@bths.edu>
2017-11-20 23:15:01 -0500

    added idea for ai (last idea) and powerup may be scratched.

--- Text Adventure/src/ethanDavidMinigame/EthanAlarmAI.java
+++ Text Adventure/src/ethanDavidMinigame/EthanAlarmAI.java
@@ -2,7 +2,7 @@ package ethanDavidMinigame;
 
 public class EthanAlarmAI{
 	boolean isActivated;
-	//AI that will combat USER
+	//AI starts to use lasers to "trap" the user?
 
 	public EthanAlarmAI(boolean isActivated) {
 		this.isActivated = isActivated;
