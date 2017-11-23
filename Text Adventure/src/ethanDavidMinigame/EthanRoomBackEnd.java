7dc3d9c59856c8082c3ad1ee709f114dbcaa4455
Lenovo <Lenovo@Jenny>
2017-11-23 18:54:34 -0500

    Updated

--- Text Adventure/src/ethanDavidMinigame/EthanRoomBackEnd.java
+++ Text Adventure/src/ethanDavidMinigame/EthanRoomBackEnd.java
@@ -17,7 +17,7 @@ public class EthanRoomBackEnd implements DavidSupport{
 	
 	public EthanRoomBackEnd(EthanSupport frontend) {
 		this.frontend = frontend;
-		MONEY_CUT_OFF = 100000;
+		MONEY_CUT_OFF = 50000;
 		currMoney = 0;
 		lost = false;
 	}
