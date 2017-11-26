432acc34173f87a77bb56e13515bfb7d0d094692
Jason's Pc <Jason's Pc@Jasons-Desktop>
2017-11-26 18:15:10 -0500

    efficancy

--- Text Adventure/src/jasonYJasonZMinigame/JasonYSupport.java
+++ Text Adventure/src/jasonYJasonZMinigame/JasonYSupport.java
@@ -3,7 +3,6 @@ package jasonYJasonZMinigame;
 import java.awt.event.KeyEvent;
 
 public interface JasonYSupport {
-	void kickOff();
 	void increaseKillCount();
 	void changeDifficulty(int i);
 	void validInput(String input);
