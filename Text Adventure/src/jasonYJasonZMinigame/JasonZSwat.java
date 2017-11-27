2eaed90b2b830bd6ba18378e763eecd7b6b233b0
yangj <yangj@LAPTOP-HMQEEB7B.fios-router.home>
2017-11-26 23:56:38 -0500

    updates

--- Text Adventure/src/jasonYJasonZMinigame/JasonZSwat.java
+++ Text Adventure/src/jasonYJasonZMinigame/JasonZSwat.java
@@ -11,42 +11,49 @@ public class JasonZSwat extends NPC {
 	public int hp = 100;
 	public int armor = 70;
 	public int index;
+	public int row;
+	public int col;
 	
 	
-	public JasonZSwat(NPCRoom[][] floor,int index)
-	{
+	public JasonZSwat(int row, int col, NPCRoom[][] floor,int index){
 		//w.e the map is called.
-		super(floor);
+		super(row, col, floor);
 		this.index = index;
 	}
+	
 	public void makeGuns(String[] info) {
 		// random decider for guns;
 		this.gun = new JasonZGuns(info);
 	}
 	
-	public int[] calculateMove(int userRow, int userCol)
-	{
+	public int[] calculateMove(int row, int col){
 		int dir = checkDirection();
-		return possibleMoves[dir];
+		int[] newPosition = new int[2]; 
+		newPosition[0] = row + possibleMoves[dir][0];
+		newPosition[1] = col + possibleMoves[dir][1];
+		return newPosition;
 	}
+	
 	private int checkDirection() {
-		//
-		if(this.getCurrentRow() - CaveRoom.getRow() > 0)
-		{
+		if(this.getCurrentRow() - JasonZBackend.starterRow > 0){
 			return 0;
 		}
-		if(this.getCurrentRow() - CaveRoom.getRow() < 0)
-		{
-			return 3;
-		}
-		if(this.getCurrentCol() - CaveRoom.getCol() >0)
-		{
-			return 1;
-		}
-		if(this.getCurrentCol() - CaveRoom.getCol() < 0)
+		if(this.getCurrentRow() - JasonZBackend.starterRow < 0)
 		{
 			return 2;
 		}
+		if(this.getCurrentCol() - JasonZBackend.starterCol >0)
+		{
+			return 1;
+		}
+		if(this.getCurrentCol() - JasonZBackend.starterCol < 0)
+		{
+			return 3;
+		}
 		return 0;
+	}
+	
+	public String getSymbol(){
+		return "S";
 	}
 }
