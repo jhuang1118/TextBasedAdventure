7ced53d6a43c7154006617327c6a27942cca5715
yangj <yangj@LAPTOP-HMQEEB7B.fios-router.home>
2017-11-26 23:59:22 -0500

    updates

--- Text Adventure/src/jasonYJasonZMinigame/JasonZSupport.java
+++ Text Adventure/src/jasonYJasonZMinigame/JasonZSupport.java
@@ -3,17 +3,10 @@ package jasonYJasonZMinigame;
 public interface JasonZSupport {
 
 	void createMap(int size);
-	void follow();
-		//npcs in the map will move towards the player after played inputs a move
-	void rangeDisplay();
-		//colors in a number of boxes based on the range of the gun
-		//should be colored red 
 	void play();
 		//createMap();
 		//populateMap();
 		//display it all
-	void killCounter();
-		//displays number of kills
 	void copCounter();
 		//displays number of cops on the map
 	void introduction();
