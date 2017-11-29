057d4b83aa6caf069e8525961f53f25059f1335c
deisen2 <eisendaniel9@gmail.com>
2017-11-29 05:26:30 -0500

    asdf

--- Text Adventure/src/johnsonDanielMinigame/DanielLockerGame.java
+++ Text Adventure/src/johnsonDanielMinigame/DanielLockerGame.java
@@ -1,168 +1,148 @@
 package johnsonDanielMinigame;
 
-import java.util.Scanner;
+import java.util.*;
 import caveExplorer.CaveExplorer;
 
 public class DanielLockerGame {
 
-	private DanielLocker[][] theMat;
-	private DanielLocker[][] theLockers;
-	private boolean onLocker;
-	int[] rows = {1,3};
-	int[] cols = {1,3,5};
-	int ROWTOT = 6;
-	int COLTOT = 6;
-	private int newPsn;
-	private int rowSelected;
-	private int colSelected;
-	public DanielLockerGame() {
-		theMat = new DanielLocker[6][6];
-		onLocker = false;
-		makeLocker(theLockers,1,1);
-		makeLocker(theLockers,1,3);
-		makeLocker(theLockers,1,5);
-		makeLocker(theLockers,3,1);
-		makeLocker(theLockers,3,3);
-		makeLocker(theLockers,3,5);
-	}
-	
-	public boolean isOnLocker() {
-		return onLocker;
-	}
+    private DanielLocker[][] theLockers;
+    private boolean onLocker;
+    final int ROWTOT = 3;
+    final int COLTOT = 2;
+    private int newPsn;
+    public DanielLockerGame() {
+        theLockers = new DanielLocker[ROWTOT][COLTOT];
+        onLocker = false;
+        for(int ri = 0; ri < ROWTOT; ri++) {
+            for(int ci = 0; ci < COLTOT; ci++) {
+                makeLocker(ri,ci);
+            }
+        }
+        createThing();
+    }
+    
+    public boolean isOnLocker() {
+        return onLocker;
+    }
 
-	public void setOnLocker(boolean onLocker) {
-		this.onLocker = onLocker;
-	}
+    public void setOnLocker(boolean onLocker) {
+        this.onLocker = onLocker;
+    }
 
-	public static void main(String[] args) {
-		DanielLockerGame demo = new DanielLockerGame();
-		demo.play();
-	}
+    public static void main(String[] args) {
+        DanielLockerGame demo = new DanielLockerGame();
+        demo.play();
+    }
 
-	private void makeLocker(DanielLocker[][] arr,int row, int col) {
-		arr[row][col] = new DanielLocker();
-	}
-	public void play() {
-		rowSelected = generateRandNum(0,rows.length-1);
-		colSelected = generateRandNum(0,cols.length-1);
-		
-		createThing(rowSelected, colSelected);
-				
-		System.out.println("In this part of the game you will pick coordinates around the map and go to lockers. You will have only 3 keys and each key can open any locker only once. "
-				+ "These lockers contain either the person you are looking for, a bomb, or"
-				+ " nothing.");
-		int keys = theLockers.length / 2;
-		while(keys > 0) {
-		//	checkWon();
-			displayBoard();
-			displayKeysLeft(keys);
-			String input = getValidUserInput();
-			//answer(input);
-			keys--;
-		}
-		printGG();
-	}
+    private void makeLocker(int row, int col) {
+        theLockers[row][col] = new DanielLocker();
+    }
+    public void play() {
+        int rowSelected = generateRandNum(0,ROWTOT-1);
+        int colSelected = generateRandNum(0,COLTOT-1);
+                
+        System.out.println("In this part of the game you will walk around the map and go to lockers. You will have only 3 keys and each key can open any locker only once. "
+                + "These lockers contain either the person you are looking for, a bomb, or"
+                + " nothing. When you walk up to a locker, press 'x'");
+        int keys = ROWTOT;
+        while(keys > 0) {
+            displayBoard();
+            displayKeysLeft(keys);
+            String input = getValidUserInput();
+            //answer(input);
+            keys--;
+        }
+        printGG();
+    }
 
-	
-	/*private void checkWon() {
-		if()
-		
-	}
-	*/
+    
+    private void createThing() {
+        int personRowSelected = generateRandNum(0, ROWTOT -1);
+        int personColSelected = generateRandNum(0, COLTOT -1);
+        int bombRowSelected = 0;
+        int bombColSelected = 0;
+        
+        theLockers[personRowSelected][personColSelected].setPerson(true);
+        theLockers[personRowSelected][personColSelected].setEmpty(false);
 
-	private void createThing(int rowSelected, int colSelected) {
-		theLockers[rowSelected][colSelected].setPerson(true);
-		theLockers[rowSelected][colSelected].setEmpty(false);
-		
-		int[] options = generateRandNumExcept(rowSelected, colSelected);
-		theLockers[options[0]][options[1]].setBomb(true);
-		theLockers[options[0]][options[1]].setEmpty(false);
-	}
+        do { 
+            bombRowSelected = generateRandNum(0, ROWTOT -1);
+            bombColSelected = generateRandNum(0, COLTOT -1);
+        } while(personRowSelected != bombRowSelected && personColSelected != bombColSelected);
+        
+        theLockers[bombRowSelected][bombColSelected].setBomb(true);
+        theLockers[bombRowSelected][bombColSelected].setEmpty(false);
+    }
 
-	private int generateRandNum(int min, int max) {
-		return (int)(Math.random()*(max - min + 1));		
-	}
-	
-	private int[] generateRandNumExcept(int exceptRow, int exceptCol) {
-		int[] arr  = new int[2]; 
-		int bombRow = generateRandNum(0,rows.length-1);
-		int bombCol = generateRandNum(0,cols.length-1);
-		
-		while(bombRow == exceptRow && bombCol == exceptCol) {
-			bombRow = generateRandNum(0,rows.length-1);
-			bombCol = generateRandNum(0,cols.length-1);
-		}
-		arr[0] = bombRow;
-		arr[1] = bombCol;
-		
-		return arr;
-	}
+    private int generateRandNum(int min, int max) {
+        return (int)(Math.random()*(max - min + 1));        
+    }
 
-	private void printGG() {
-		System.out.println("You Lose :(");		
-	}
+    private void printGG() {
+        System.out.println("You Lose :(Ha, ha)");        
+    }
 
-	private String getValidUserInput() {
-		String input = CaveExplorer.in.nextLine();
-		
-		while(!validInput(input)) {
-			System.out.print("Please enter coordinates in the form row, col without any parenthesis or spaces. Remember the minimum row and columumn is 0 and the max is 5.");
-			input = CaveExplorer.in.nextLine();
-			return input;
-		}
-		
-		return input;
-	
-	}
+    private String getValidUserInput() {
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a cell with the top left being 0,0 : row,col. ");
+        String input = reader.next(); // Scans the next token of the input as an int.
+        System.out.println("number entered: " + input);
+        //once finished
+        reader.close(); 
+        return input;
+    }
 
-	private boolean validInput(String input) {
-		String[] splitInput = input.split(",");
-		if(splitInput.length == 2) {
-			return Integer.valueOf(splitInput[0]) > 0 && Integer.valueOf(splitInput[0]) < 6 && Integer.valueOf(splitInput[1]) > 0 && Integer.valueOf(splitInput[0]) < 6; 
-		}
-		return false;
-	}
+    private boolean validInput(String input) {
+        String[] splitInput = input.split(",");
+        if(splitInput.length == 2) {
+            return Integer.valueOf(splitInput[0]) > 0 && Integer.valueOf(splitInput[0]) < 6 && Integer.valueOf(splitInput[1]) > 0 && Integer.valueOf(splitInput[0]) < 6; 
+        }
+        return false;
+    }
 
-	public boolean isOnLocker(int row, int col) {
-		int[][] lockerpsns = {{1,1},{1,3},{1,5},{3,1},{3,3},{3,5}};
-		int[][] personpsn = {{row,col}};
-		int count = 0;
-		for(int i = 0; i < lockerpsns.length; i++) {
-			if(lockerpsns[i].equals(personpsn[0])) {
-				count++;
-			}
-		}
-		if(count > 0) {
-			return true;
-		}
-		return false;
-	
-	}
-	private void answer(String input) {
+    public boolean isOnLocker(int row, int col) {
+        int[][] lockerpsns = {{1,1},{1,3},{1,5},{3,1},{3,3},{3,5}};
+        int[][] personpsn = {{row,col}};
+        int count = 0;
+        for(int i = 0; i < lockerpsns.length; i++) {
+            if(lockerpsns[i].equals(personpsn[0])) {
+                count++;
+            }
+        }
+        if(count > 0) {
+            return true;
+        }
+        return false;
+    
+    }
+    private void answer(String input) {
+        String[] splitInput = input.split(",");
+        Integer coor1 = Integer.valueOf(splitInput[0]);
+        Integer coor2 = Integer.valueOf(splitInput[1]);
+        if(isOnLocker(coor1,coor2)) {
+            theLockers[coor1][coor2].setOpen(true);
+            johnsonDanielMinigame.JohnsonUnlockGame.main(null);
 
+        }
+    }
 
-		String[] splitInput = input.split(",");
-		Integer coor1 = Integer.valueOf(splitInput[0]);
-		Integer coor2 = Integer.valueOf(splitInput[1]);
-		if(isOnLocker(coor1,coor2)) {
-			theLockers[coor1][coor2].setOpen(true);
-			johnsonDanielMinigame.JohnsonUnlockGame.main(null);
+    private boolean isPossibleMove(int[] is) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-		}
-	}
-	
+    private void displayKeysLeft(int keys) {
+        System.out.println("You have " + keys + " keys left.");
+    }
 
-	private void displayKeysLeft(int keys) {
-		System.out.println("You have " + keys + " keys left.");
-	}
-
-	private void displayBoard() {
-		for(int row = 0; row < ROWTOT; row++) {
-			for(int col = 0; col < COLTOT; col++) {
-				System.out.print(theLockers[row][col]);
-			}
-			System.out.println(" ");
-		}
-	}
+    private void displayBoard() {
+        for(int row = 0; row < ROWTOT; row++) {
+            for(int col = 0; col < COLTOT; col++) {
+                System.out.print(theLockers[row][col]);
+            }
+            System.out.println(" ");
+        }
+    }
 
 }
+
